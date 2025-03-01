864a5ac6657eee2b47ca3c63a99638427ddc4449
ahmed samy <45830136+ahmedsamy33@users.noreply.github.com>
2025-03-01 10:42:41 +0200

    git commit init

---
+++ assets/side-bar-menu/menu-data.ts
@@ -0,0 +1,16 @@
+export const menuData = [
+  {
+    "id": "0",
+    "label": "Dashboard",
+    "data": "/dashboard",
+    "iconPath": "assets/icons/home.svg",
+    "children": []
+  },
+  {
+    "id": "1",
+    "label": "User Management",
+    "data": "/user-management",
+    "iconPath": "assets/icons/userGroup.svg",
+    "children": []
+  }
+]
